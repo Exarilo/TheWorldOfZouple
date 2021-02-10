cca0f226faa2502fe41fc4a9dff32754ee32ff2c
Madery Romain <72389130+Exarilo@users.noreply.github.com>
2021-02-10 12:11:24 +0100

    changement

--- app/src/main/java/com/example/theworldofzouple/FullscreenActivity.java
+++ app/src/main/java/com/example/theworldofzouple/FullscreenActivity.java
@@ -5,16 +5,19 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullscreenActivity extends AppCompatActivity {
+    private Button btChangeActivity;
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -103,11 +106,23 @@ public class FullscreenActivity extends AppCompatActivity {
                 toggle();
             }
         });
+        btChangeActivity= findViewById(R.id.btPlay);
+
+        btChangeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToSecondActivity();
+            }
+        });
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
 //          findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+    }
+    private void moveToSecondActivity(){
+        Intent intent =new Intent(FullscreenActivity.this,SecondActivity.class);
+        startActivity(intent);
     }
 
     @Override
