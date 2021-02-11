a64217e70dfc2fd8cb0143eeebc300c197a2e151
Madery Romain <72389130+Exarilo@users.noreply.github.com>
2021-02-11 12:44:52 +0100

    MenuCaracter

---
+++ app/src/main/java/com/example/theworldofzouple/CaracterActivity.java
@@ -0,0 +1,14 @@
+package com.example.theworldofzouple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaracterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caracter);
+    }
+}
--- app/src/main/java/com/example/theworldofzouple/SecondActivity.java
+++ app/src/main/java/com/example/theworldofzouple/SecondActivity.java
@@ -2,14 +2,45 @@ package com.example.theworldofzouple;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SecondActivity extends AppCompatActivity {
-
+    private Button btMenuCaracter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+        btMenuCaracter= findViewById(R.id.btCaracter);
+        btMenuCaracter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToMenuCaracter();
+            }
+        });
+
+
+
+
+
+        btMenuCaracter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToMenuCaracter();
+            }
+        });
+
+
+    }
+
+    private void moveToMenuCaracter(){
+        Intent intent =new Intent(SecondActivity.this,CaracterActivity.class);
+        startActivity(intent);
     }
 }
